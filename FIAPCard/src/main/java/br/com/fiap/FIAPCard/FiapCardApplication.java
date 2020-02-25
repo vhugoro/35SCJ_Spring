a58bf578beffc4aac2a83f378fba7b4f7556127e
Victor Hugo <vhugoro@gmail.com>
2020-02-24 23:45:12 -0300

    Criação Projeto FiapCard - Avaliação Spring

---
+++ FIAPCard/src/main/java/br/com/fiap/FIAPCard/FiapCardApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FIAPCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapCardApplication.class, args);
+	}
+
+}
---
+++ FIAPCard/src/test/java/br/com/fiap/FIAPCard/FiapCardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FIAPCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
