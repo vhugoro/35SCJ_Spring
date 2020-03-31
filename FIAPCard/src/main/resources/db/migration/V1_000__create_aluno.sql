7f42f10479948a5430d38cd49883afd2eb102555
Carlos Eduardo Zanchetta <cazanchetta@gmail.com>
2020-03-30 21:59:49 -0300

    adicionado o H2

--- FIAPCard/src/main/resources/db/migration/V1_000__create_aluno.sql
+++ FIAPCard/src/main/resources/db/migration/V1_000__create_aluno.sql
@@ -1,7 +1,8 @@
-CREATE TABLE ALUNO (
+CREATE TABLE TB_ALUNO (
   id integer NOT NULL AUTO_INCREMENT,
   nome varchar(200) NOT NULL,
-  cpf long NOT NULL,
-  data_criacao timestamp DEFAULT NULL,
-  data_atualizacao timestamp DEFAULT NULL
+  cpf bigint NOT NULL,
+  created_date timestamp DEFAULT NULL,
+  updated_date timestamp DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
